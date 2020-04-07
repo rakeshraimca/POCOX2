075d69799c0ffca249189669d464af0d73590ad8
Hardrock_83 <rakeshraimca@gmail.com>
2020-04-07 22:56:55 +0530

    some fixes for anxiety scheduler and wakelock

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -571,7 +571,8 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 // AP: Function to check if a wakelock is on the wakelock blocker list
 static bool check_for_block(struct wakeup_source *ws)
 {
-	char wakelock_name[52];
+	char wakelock_name[52] = {0};
+	int length;
 
 	// if debug mode on, print every wakelock requested
 	if (wl_blocker_debug)
@@ -581,27 +582,39 @@ static bool check_for_block(struct wakeup_source *ws)
 	if (!wl_blocker_active)
 		return false;
 
-	// check if wakelock is in wake lock list to be blocked
+	// only if ws structure is valid
 	if (ws)
 	{
-		
-	        // wake lock names handled have maximum length=50 and minimum=1
+		// wake lock names handled have maximum length=50 and minimum=1
 		length = strlen(ws->name);
-		if ((length > 50) || (length < 1))	
-		return false;
+		if ((length > 50) || (length < 1))
+			return false;
 
+		// check if wakelock is in wake lock list to be blocked
 		sprintf(wakelock_name, ";%s;", ws->name);
 
 		if(strstr(list_wl_search, wakelock_name) == NULL)
 			return false;
+
+		// wake lock is in list, print it if debug mode on
+		if (wl_blocker_debug)
+			printk("Boeffla WL blocker: %s blocked\n", ws->name);
+
+		// if it is currently active, deactivate it immediately + log in debug mode
+		if (ws->active)
+		{
+			wakeup_source_deactivate(ws);
+
+			if (wl_blocker_debug)
+				printk("Boeffla WL blocker: %s killed\n", ws->name);
+		}
+
+		// finally block it
+		return true;
 	}
 
-	// wake lock is in list, print it if debug mode on
-	if (wl_blocker_debug)
-		printk("Boeffla WL blocker: %s blocked\n", ws->name);
-
-	// finally block it
-	return true;
+	// there was no valid ws structure, do not block by default
+	return false;
 }
 #endif
 
@@ -627,6 +640,7 @@ static void wakeup_source_report_event(struct wakeup_source *ws, bool hard)
 
 	if (hard)
 		pm_system_wakeup();
+	#ifdef CONFIG_BOEFFLA_WL_BLOCKER
 
 	}
 	#endif
